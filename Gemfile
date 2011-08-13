5b5d7690615b19843331e0faf5f5bf439d8e1ff8
Jason <devilshaircut@gmail.com>
2011-08-12 23:18:29 -0400

    Start priceboard project.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'pg'
+gem "compass", ">= 0.11.3"
+gem 'rake', '~>0.8.7' # rake 0.9 breaks rails
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Priceboard::Application.config.secret_token = 'af7e56ff0005fc33e47922db0823021f31bcf6bcf0891c60411bc7bcb0a78a70274ba37f5ce644c33afa3f907579cf57fb5c438167c4e81cc161f6c6ac59ac61'
